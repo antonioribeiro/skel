52628094de6ef88eb5c06855c929bd254d20d6c3
Antonio Carlos Ribeiro <acr@antoniocarlosribeiro.com>
2015-03-12 17:50:46 -0300

    Upgrade skeleton to Laravel 5

--- src/PackageNameServiceProvider.php
+++ src/PackageNameServiceProvider.php
@@ -22,6 +22,66 @@ class :PackageNameServiceProvider extends ServiceProvider {
 	}
 
 	/**
+	 * Boot the service provider.
+	 *
+	 * @return void
+	 */
+	public function boot()
+	{
+		$this->publishViews();
+
+		$this->publishTranslations();
+
+		$this->publishConfiguration();
+
+		$this->publishMigrations();
+	}
+
+	/**
+	 * Load package views.
+	 *
+	 */
+	protected function publishViews()
+	{
+		$this->loadViewsFrom(__DIR__ . '/views', ':package_name');
+
+		$this->publishes([
+			__DIR__ . '/views' => base_path('resources/views/vendor/:package_name'),
+		]);
+	}
+
+	/**
+	 * Load package translations.
+	 *
+	 */
+	protected function publishTranslations()
+	{
+		$this->loadTranslationsFrom(__DIR__.'/lang', ':package_name');
+	}
+
+	/**
+	 * Publish configuration.
+	 *
+	 */
+	protected function publishConfiguration()
+	{
+		$this->publishes([
+		    __DIR__.'/config/config.php' => config_path(':package_name.php'),
+		]);
+	}
+
+	/**
+	 * Publish configuration.
+	 *
+	 */
+	protected function publishMigrations()
+	{
+		$this->publishes([
+			__DIR__.'/database/migrations/' => base_path('/database/migrations')
+		], 'migrations');
+	}
+
+	/**
 	 * Get the services provided by the provider.
 	 *
 	 * @return array

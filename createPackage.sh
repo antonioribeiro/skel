1f7e4deee1ff48bc864d3ae080a644df69920275
Antonio Carlos Ribeiro <acr@antoniocarlosribeiro.com>
2015-03-12 17:32:28 -0300

    Improve code quality

--- createPackage.sh
+++ createPackage.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -e
+
 # Export those variables in your .bashrc to ease the deployment of new packages
 #
 # export VENDOR_NAME=pragmarx
@@ -64,43 +66,43 @@ function generateDefaultData()
 
 function searchAndReplace()
 {
-    find $1 -name "*" -exec sed -i "s/$2/$3/g" {} \; &> /dev/null
+    find "$1" -name "*" -exec sed -i "s/$2/$3/g" {} \; &> /dev/null
 }
 
 function createPackage()
 {
-    message Creating package $VENDOR_NAME/$PACKAGE_NAME
-    message From repository: $SKELETON_REPOSITORY branch $SKELETON_PACKAGE_BRANCH
+    message Creating package "$VENDOR_NAME/$PACKAGE_NAME"
+    message From repository: "$SKELETON_REPOSITORY" branch "$SKELETON_PACKAGE_BRANCH"
 
-    git clone --branch $SKELETON_PACKAGE_BRANCH $SKELETON_REPOSITORY $DESTINATION_FOLDER
+    git clone --branch "$SKELETON_PACKAGE_BRANCH" "$SKELETON_REPOSITORY" "$DESTINATION_FOLDER"
 
-    searchAndReplace $DESTINATION_FOLDER $SKELETON_PACKAGE_NAME             $PACKAGE_NAME
-    searchAndReplace $DESTINATION_FOLDER $SKELETON_PACKAGE_NAME_CAPITAL     $PACKAGE_NAME_CAPITAL
-    searchAndReplace $DESTINATION_FOLDER $SKELETON_VENDOR_NAME              $VENDOR_NAME
-    searchAndReplace $DESTINATION_FOLDER $SKELETON_VENDOR_NAME_CAPITAL      $VENDOR_NAME_CAPITAL
-    searchAndReplace $DESTINATION_FOLDER $SKELETON_AUTHOR_NAME              $PACKAGE_AUTHOR_NAME
-    searchAndReplace $DESTINATION_FOLDER $SKELETON_AUTHOR_EMAIL             $PACKAGE_AUTHOR_EMAIL
-    searchAndReplace $DESTINATION_FOLDER $SKELETON_AUTHOR_WEBSITE           $PACKAGE_AUTHOR_WEBSITE
-    searchAndReplace $DESTINATION_FOLDER $SKELETON_AUTHOR_USERNAME          $PACKAGE_AUTHOR_USERNAME
-    searchAndReplace $DESTINATION_FOLDER $SKELETON_DESCRIPTION              $PACKAGE_DESCRIPTION
+    searchAndReplace "$DESTINATION_FOLDER" "$SKELETON_PACKAGE_NAME"             "$PACKAGE_NAME"
+    searchAndReplace "$DESTINATION_FOLDER" "$SKELETON_PACKAGE_NAME_CAPITAL"     "$PACKAGE_NAME_CAPITAL"
+    searchAndReplace "$DESTINATION_FOLDER" "$SKELETON_VENDOR_NAME"              "$VENDOR_NAME"
+    searchAndReplace "$DESTINATION_FOLDER" "$SKELETON_VENDOR_NAME_CAPITAL"      "$VENDOR_NAME_CAPITAL"
+    searchAndReplace "$DESTINATION_FOLDER" "$SKELETON_AUTHOR_NAME"              "$PACKAGE_AUTHOR_NAME"
+    searchAndReplace "$DESTINATION_FOLDER" "$SKELETON_AUTHOR_EMAIL"             "$PACKAGE_AUTHOR_EMAIL"
+    searchAndReplace "$DESTINATION_FOLDER" "$SKELETON_AUTHOR_WEBSITE"           "$PACKAGE_AUTHOR_WEBSITE"
+    searchAndReplace "$DESTINATION_FOLDER" "$SKELETON_AUTHOR_USERNAME"          "$PACKAGE_AUTHOR_USERNAME"
+    searchAndReplace "$DESTINATION_FOLDER" "$SKELETON_DESCRIPTION"              "$PACKAGE_DESCRIPTION"
 
-    renameAll $DESTINATION_FOLDER $SKELETON_PACKAGE_NAME                    $PACKAGE_NAME
-    renameAll $DESTINATION_FOLDER $SKELETON_PACKAGE_NAME_CAPITAL            $PACKAGE_NAME_CAPITAL
-    renameAll $DESTINATION_FOLDER $SKELETON_VENDOR_NAME                     $VENDOR_NAME
-    renameAll $DESTINATION_FOLDER $SKELETON_VENDOR_NAME_CAPITAL             $VENDOR_NAME_CAPITAL
+    renameAll "$DESTINATION_FOLDER" "$SKELETON_PACKAGE_NAME"                    "$PACKAGE_NAME"
+    renameAll "$DESTINATION_FOLDER" "$SKELETON_PACKAGE_NAME_CAPITAL"            "$PACKAGE_NAME_CAPITAL"
+    renameAll "$DESTINATION_FOLDER" "$SKELETON_VENDOR_NAME"                     "$VENDOR_NAME"
+    renameAll "$DESTINATION_FOLDER" "$SKELETON_VENDOR_NAME_CAPITAL"             "$VENDOR_NAME_CAPITAL"
 
-    mv $DESTINATION_FOLDER/src/PackageNameClass.php $DESTINATION_FOLDER/src/$PACKAGE_NAME_CAPITAL.php
-    mv $DESTINATION_FOLDER/src/PackageNameServiceProvider.php $DESTINATION_FOLDER/src/${PACKAGE_NAME_CAPITAL}ServiceProvider.php
-    rm -rf $DESTINATION_FOLDER/.git/
+    mv "$DESTINATION_FOLDER/src/PackageNameClass.php" "$DESTINATION_FOLDER/src/$PACKAGE_NAME_CAPITAL.php"
+    mv "$DESTINATION_FOLDER/src/PackageNameServiceProvider.php" "$DESTINATION_FOLDER/src/${PACKAGE_NAME_CAPITAL}ServiceProvider.php"
+    rm -rf "$DESTINATION_FOLDER/.git/"
 
-    cd $DESTINATION_FOLDER
+    cd "$DESTINATION_FOLDER"
 
     git init
     git add -A
     git commit -m "first commit"
 
     if [[ "$PACKAGE_REPOSITORY" != "" ]]; then
-        git remote add origin $PACKAGE_REPOSITORY
+        git remote add origin "$PACKAGE_REPOSITORY"
 
         git push origin master
 
@@ -110,11 +112,11 @@ function createPackage()
 
 function askForData()
 {
-    DESTINATION_FOLDER=`pwd`/
-    inquireText "Package destination folder:" $DESTINATION_FOLDER
+    DESTINATION_FOLDER=$(pwd)/
+    inquireText "Package destination folder:" "$DESTINATION_FOLDER"
     DESTINATION_FOLDER=$answer
 
-    if [[ -d $DESTINATION_FOLDER ]]; then
+    if [[ -d "$DESTINATION_FOLDER" ]]; then
         message
         message "Destination folder already exists. Aborting..."
         message
@@ -123,12 +125,12 @@ function askForData()
 
     message
 
-    VENDOR_NAME=`echo $VENDOR_NAME | awk '{print tolower($0)}'`
-    inquireText "Vendor name for your new package (lowercase):" $VENDOR_NAME
+    VENDOR_NAME=$(echo "$VENDOR_NAME" | awk '{print tolower($0)}')
+    inquireText "Vendor name for your new package (lowercase):" "$VENDOR_NAME"
     VENDOR_NAME=$answer
 
     VENDOR_NAME_CAPITAL=${VENDOR_NAME^}
-    inquireText "Vendor name for your new package (Capitalized):" $VENDOR_NAME_CAPITAL
+    inquireText "Vendor name for your new package (Capitalized):" "$VENDOR_NAME_CAPITAL"
     VENDOR_NAME_CAPITAL=$answer
 
     message
@@ -139,47 +141,47 @@ function askForData()
         VCS_USER=$VENDOR_NAME
     fi
 
-    inquireText "Your VCS username:" $VCS_USER
+    inquireText "Your VCS username:" "$VCS_USER"
     VCS_USER=$answer
 
     message
 
-    PACKAGE_NAME=`echo $(basename $DESTINATION_FOLDER) | awk '{print tolower($0)}'`
-    inquireText "Your new package name (lowercase):" $PACKAGE_NAME
+    PACKAGE_NAME=$(echo "$(basename "$DESTINATION_FOLDER")" | awk '{print tolower($0)}')
+    inquireText "Your new package name (lowercase):" "$PACKAGE_NAME"
     PACKAGE_NAME=$answer
 
     PACKAGE_NAME_CAPITAL=${PACKAGE_NAME^}
-    inquireText "Your new package name (Capitalized):" $PACKAGE_NAME_CAPITAL
+    inquireText "Your new package name (Capitalized):" "$PACKAGE_NAME_CAPITAL"
     PACKAGE_NAME_CAPITAL=$answer
 
     if [[ "$PACKAGE_AUTHOR_NAME" == "" ]]; then
         PACKAGE_AUTHOR_NAME=$VENDOR_NAME_CAPITAL
     fi
-    inquireText "Author name:" $VENDOR_NAME_CAPITAL
+    inquireText "Author name:" "$VENDOR_NAME_CAPITAL"
     PACKAGE_AUTHOR_NAME=$answer
 
-    inquireText "Author email:" $PACKAGE_AUTHOR_EMAIL
+    inquireText "Author email:" "$PACKAGE_AUTHOR_EMAIL"
     PACKAGE_AUTHOR_EMAIL=$answer
 
-    inquireText "Author website:" $PACKAGE_AUTHOR_WEBSITE
+    inquireText "Author website:" "$PACKAGE_AUTHOR_WEBSITE"
     PACKAGE_AUTHOR_WEBSITE=$answer
 
-    inquireText "Author username:" $VCS_USER
+    inquireText "Author username:" "$VCS_USER"
     PACKAGE_AUTHOR_USERNAME=$answer
 
-    inquireText "Description:" $PACKAGE_DESCRIPTION
+    inquireText "Description:" "$PACKAGE_DESCRIPTION"
     PACKAGE_DESCRIPTION=$answer
 
     PACKAGE_REPOSITORY=https://$SKELETON_VCS_SERVICE/$VCS_USER/$PACKAGE_NAME.git
-    inquireText "Your new package repository link (create a package first or leave it blank):" $PACKAGE_REPOSITORY
+    inquireText "Your new package repository link (create a package first or leave it blank):" "$PACKAGE_REPOSITORY"
     PACKAGE_REPOSITORY=$answer
 
     message
     message --- Skeleton replacements
     message
 
-    SKELETON_VENDOR_NAME=`echo $SKELETON_VENDOR_NAME | awk '{print tolower($0)}'`
-    inquireText "Skeleton Vendor name (lowercase):" $SKELETON_VENDOR_NAME
+    SKELETON_VENDOR_NAME=$(echo "$SKELETON_VENDOR_NAME" | awk '{print tolower($0)}')
+    inquireText "Skeleton Vendor name (lowercase):" "$SKELETON_VENDOR_NAME"
     SKELETON_VENDOR_NAME=$answer
 
     if [[ "$SKELETON_VENDOR_NAME" != ":vendor_name" ]]; then
@@ -187,13 +189,13 @@ function askForData()
     fi
 
     SKELETON_VENDOR_NAME_CAPITAL=${SKELETON_VENDOR_NAME_CAPITAL^}
-    inquireText "Skeleton Vendor name (Capitalized):" $SKELETON_VENDOR_NAME_CAPITAL
+    inquireText "Skeleton Vendor name (Capitalized):" "$SKELETON_VENDOR_NAME_CAPITAL"
     SKELETON_VENDOR_NAME_CAPITAL=$answer
 
     message 
 
-    SKELETON_PACKAGE_NAME=`echo $(basename $SKELETON_PACKAGE_NAME) | awk '{print tolower($0)}'`
-    inquireText "Skeleton package name (lowercase):" $SKELETON_PACKAGE_NAME
+    SKELETON_PACKAGE_NAME=$(echo "$(basename "$SKELETON_PACKAGE_NAME")" | awk '{print tolower($0)}')
+    inquireText "Skeleton package name (lowercase):" "$SKELETON_PACKAGE_NAME"
     SKELETON_PACKAGE_NAME=$answer
 
     if [[ "$SKELETON_PACKAGE_NAME_CAPITAL" != ":PackageName" ]]; then
@@ -201,7 +203,7 @@ function askForData()
     fi
 
     SKELETON_PACKAGE_NAME_CAPITAL=${SKELETON_PACKAGE_NAME_CAPITAL^}
-    inquireText "Skeleton package name (Capitalized):" $SKELETON_PACKAGE_NAME_CAPITAL
+    inquireText "Skeleton package name (Capitalized):" "$SKELETON_PACKAGE_NAME_CAPITAL"
     SKELETON_PACKAGE_NAME_CAPITAL=$answer
 
     inquireText "Skeleton author name:" :author_name
@@ -229,13 +231,13 @@ function askForData()
     fi
 
     if [[ "$repository" == "" ]]; then
-        repo=$SKELETON_VENDOR_NAME/skeleton.git
+        repo="$SKELETON_VENDOR_NAME/skeleton.git"
         if [[ "$SKELETON_VENDOR_NAME" == ":vendor_name" ]]; then
             repo=
         fi
 
         SKELETON_REPOSITORY=https://$SKELETON_VCS_SERVICE/$repo
-        inquireText "Skeleton repository link:" $SKELETON_REPOSITORY
+        inquireText "Skeleton repository link:" "$SKELETON_REPOSITORY"
         SKELETON_REPOSITORY=$answer
 
         inquireText "Skeleton branch:" master
@@ -280,9 +282,9 @@ function selectSkeletonRepository()
 
 function renameAll()
 {
-    renameFiles $1 $2 $3
+    renameFiles "$1" "$2" "$3"
 
-    renameDirectories $1 $2 $3
+    renameDirectories "$1" "$2" "$3"
 }
 
 function renameFiles()
@@ -291,14 +293,14 @@ function renameFiles()
     OLD=$2
     NEW=$3
 
-    find $FOLDER -type f -print0 | while IFS= read -r -d $'\0' file; do
-        dir=$(dirname $file)
-        filename=$(basename $file)
-        new=$dir/$(echo $filename | sed -e "s/$OLD/$NEW/g")
+    find "$FOLDER" -type f -print0 | while IFS= read -r -d $'\0' file; do
+        dir=$(dirname "$file")
+        filename=$(basename "$file")
+        new=$dir/$(echo "$filename" | sed -e "s/$OLD/$NEW/g")
 
         if [[ "$file" != "$new" ]]; then
             if [[ -f $file ]]; then
-                mv $file $new
+                mv "$file" "$new"
             fi
         fi
     done    
@@ -310,18 +312,19 @@ function renameDirectories()
     OLD=$2
     NEW=$3
 
-    find $FOLDER -type d -print0 | while IFS= read -r -d $'\0' file; do
-        new=$(echo $file | sed -e "s/$OLD/$NEW/g")
+    find "$FOLDER" -type d -print0 | while IFS= read -r -d $'\0' file; do
+        new=$(echo "$file" | sed -e "s/$OLD/$NEW/g")
 
         if [[ "$file" != "$new" ]]; then
             if [[ -d $file ]]; then
-                mv $file $new
+                mv "$file" "$new"
             fi
         fi
     done    
 }
 
-function inquireText()  {
+function inquireText()
+{
     answer=""
     value=$2
     
@@ -329,7 +332,7 @@ function inquireText()  {
         value=PragmaRX
     fi
 
-    if [[ $BASH_VERSION > '3.9' ]]; then
+    if [[ ${BASH_VERSION} > "3.9" ]]; then
         read -e -p "$1 " -i "$value" answer
     else
         read -e -p "$1 [hit enter for $value] " answer
@@ -387,4 +390,4 @@ function displayAppName()
     echo
 }
 
-main $@
+main "$@"
